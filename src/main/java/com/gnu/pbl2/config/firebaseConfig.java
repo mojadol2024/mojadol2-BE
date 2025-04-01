562aca103564dfff66dcfba6e6dc7ca68e962996
변경태 <byeongyeongtae@byeongyeongtaeui-MacBookPro.local>
2025-04-01 23:17:26 +0900

    fix:ㅜㅜ 눈이 침침해요

--- src/main/java/com/gnu/pbl2/config/firebaseConfig.java
+++ src/main/java/com/gnu/pbl2/config/firebaseConfig.java
@@ -17,30 +17,30 @@ import java.util.Map;
 public class firebaseConfig {
     // 혹시나 알림 서비스 할까 싶어서 넣어놓음 안하면 지울게요
 
-    @Value("${firebase_project_id}")
-    private String firebase_project_id;
+    @Value("${firebase.project-id}")
+    private String firebaseProjectId;
 
-    @Value("${firebase_private_key_id}")
-    private String firebase_private_key_id;
+    @Value("${firebase.private-key-id}")
+    private String firebasePrivateKeyId;
 
-    @Value("${firebase_private_key}")
-    private String firebase_private_key;
+    @Value("${firebase.private-key}")
+    private String firebasePrivateKey;
 
-    @Value("${firebase_client_email}")
-    private String firebase_client_email;
+    @Value("${firebase.client-email}")
+    private String firebaseClientEmail;
 
-    @Value("${firebase_client_id}")
-    private String firebase_client_id;
+    @Value("${firebase.client-id}")
+    private String firebaseClientId;
 
     @PostConstruct
     public void initialize() throws IOException {
         Map<String, Object> firebaseConfig = Map.of(
                 "type", "service_account",
-                "project_id", firebase_project_id,
-                "private_key_id", firebase_private_key_id,
-                "private_key", firebase_private_key.replace("\\n", "\n"),
-                "client_email", firebase_client_email,
-                "client_id", firebase_client_id,
+                "project_id", firebaseProjectId,
+                "private_key_id", firebasePrivateKeyId,
+                "private_key", firebasePrivateKey.replace("\\n", "\n"),
+                "client_email", firebaseClientEmail,
+                "client_id", firebaseClientId,
                 "auth_uri", "https://accounts.google.com/o/oauth2/auth",
                 "token_uri", "https://oauth2.googleapis.com/token",
                 "auth_provider_x509_cert_url", "https://www.googleapis.com/oauth2/v1/certs",
